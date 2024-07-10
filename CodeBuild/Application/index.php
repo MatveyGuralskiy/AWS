9df11e29cdf8c6d306f6481153e6a52762c2bd4a
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-07-10 13:45:00 +0300

    Add CodeBuild

---
+++ CodeBuild/Application/index.php
@@ -0,0 +1,7 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Welcome to the Simple Website based on PHP</h1><br>";
+echo "<h2>Created by <font color=orange>Matvey Guralskiy</font></h2><br>";
+echo "<p>Your IP address is: $ip_server</p><br>";
+?>
